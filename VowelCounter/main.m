1e3e6f59e898f5dc21a2c7a5a401f3ff0c7e9fe5
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-26 19:24:16 -0500

    Initial Commit

---
+++ VowelCounter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VowelCounter
+//
+//  Created by Edwin Cardenas on 26/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
